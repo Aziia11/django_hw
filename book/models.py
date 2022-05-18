e84e8866fb517c178aee1c9139567520b31b6b36
Azia <you@example.com>
2022-05-18 21:11:20 +0600

    Django commit

--- book/models.py
+++ book/models.py
@@ -1,26 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
 class Book(models.Model):
-        GENRE_CHOICE = (
-                ("Adventure", "Adventure"),
-                ("Literary Fiction", "Literary Fiction"),
-                ("Mystery", "Mystery"),
-                ("Thriller", "Thriller"),
-                ("Historical", "Historical"),
-                ("Romance", "Romance"),
-                ("Science Fiction", "Science Fiction"),
-                ("Drama", "Drama"),
-                ("Comedy", "Comedy")
-        )
-        title = models.CharField(max_length=100)
-        description = models.TextField()
-        image = models.ImageField(upload_to="media")
-        genre = models.CharField(max_length=100, choices=GENRE_CHOICE)
-        quantity_of_page = models.PositiveIntegerField()
-        author = models.CharField(max_length=100)
-        created_date = models.DateField(auto_now_add=True)
-        updated_date = models.DateField(auto_now=True)
+    GENRE_CHOICE = (
+        ("Adventure", "Adventure"),
+        ("Literary Fiction", "Literary Fiction"),
+        ("Mystery", "Mystery"),
+        ("Thriller", "Thriller"),
+        ("Historical", "Historical"),
+        ("Romance", "Romance"),
+        ("Science Fiction", "Science Fiction"),
+        ("Drama", "Drama"),
+        ("Comedy", "Comedy")
+    )
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to="media")
+    genre = models.CharField(max_length=100, choices=GENRE_CHOICE)
+    quantity_of_page = models.PositiveIntegerField()
+    author = models.CharField(max_length=100)
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
 
-        def __str__(self):
-                return self.title
+
+    def __str__(self):
+        return self.title
--- book/views.py
+++ book/views.py
@@ -1,6 +1,6 @@
 
 from django.shortcuts import render, get_object_or_404
-from django.http import  HttpResponse
+from django.http import HttpResponse
 from .models import Book
 # Create your views here.
 def book_world(request):
