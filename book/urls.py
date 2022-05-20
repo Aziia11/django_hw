75ee89e03693b5e356adc1e5e51fb68f51b48810
Azia <you@example.com>
2022-05-20 23:39:16 +0600

    Django commit

--- book/urls.py
+++ book/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "books_url"
 urlpatterns =[
-    path('books/', views.all_books),
-    path('books/<int:id>/', views.get_book_detail),
+    path('books/', views.all_books, name="books_all_url"),
+    path('books/<int:id>/', views.get_book_detail, name="book"),
+    path('add-book/', views.add_book, name="add_book"),
 ]
--- book/views.py
+++ book/views.py
@@ -1,15 +1,27 @@
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.http import HttpResponse
-from .models import Book
+from . import models, forms
 # Create your views here.
 def book_world(request):
     return  HttpResponse("<h2>Book World<h2>")
 
 def all_books(request):
-    books = Book.objects.all()
+    books = models.Book.objects.all()
     return render(request, "books.html", {"books": books})
 
 def get_book_detail(request, id):
-    object = get_object_or_404(Book, id=id)
+    object = get_object_or_404(models.Book, id=id)
     return render(request, "book_detail.html", {"book": object})
+
+def add_book(request):
+    method = request.method
+    if method == "POST":
+        form = forms.MasterpieceForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Masterpiece Created successfully!")
+            #return redirect(reverse("books_url:books_all_url"))
+    else:
+        form = forms.MasterpieceForm()
+    return render(request, "add_books.html", {"form": form})
