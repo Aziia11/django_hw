6276a1a4d05ccab63036fba35327af7f3e56f920
Azia <you@example.com>
2022-05-24 23:39:58 +0600

    Django commit

--- book/urls.py
+++ book/urls.py
@@ -4,6 +4,18 @@ from . import views
 app_name = "books_url"
 urlpatterns =[
     path('books/', views.all_books, name="books_all_url"),
+    path('books/latest', views.book_latest, name="Latest"),
+    path('books/adventure', views.book_adventure, name="Adventure"),
+    path('books/literaryfiction', views.book_literaryfiction, name="Literary Fiction"),
+    path('books/mystery', views.book_mystery, name="Mystery"),
+    path('books/thriller', views.book_thriller, name="Thriller"),
+    path('books/historical', views.book_historical, name="Historical"),
+    path('books/romance', views.book_romance, name="Romance"),
+    path('books/sciencefiction', views.book_sciencefiction, name="Science Fiction"),
+    path('books/drama', views.book_drama, name="Drama"),
+    path('books/comedy', views.book_comedy, name="Comedy"),
     path('books/<int:id>/', views.get_book_detail, name="book"),
+    path('books/<int:id>/update/', views.book_update, name="book_update"),
+    path('books/<int:id>/delete/', views.book_delete, name="book_delete"),
     path('add-book/', views.add_book, name="add_book"),
 ]
--- book/views.py
+++ book/views.py
@@ -2,12 +2,55 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.http import HttpResponse
 from . import models, forms
+from datetime import datetime, timedelta
+
+start_date = datetime.today() - timedelta(days=3)
 # Create your views here.
 def book_world(request):
     return  HttpResponse("<h2>Book World<h2>")
 
 def all_books(request):
-    books = models.Book.objects.all()
+    books = models.Book.objects.order_by("-id")
+    return render(request, "books.html", {"books": books})
+
+def book_latest(request):
+    books = models.Book.objects.filter(created_date__gt=start_date).order_by("-id")
+    return render(request, "books.html", {"books": books})
+
+def book_adventure(request):
+    books = models.Book.objects.filter(genre="Adventure").order_by("-id")
+    return render(request, "books.html", {"books": books})
+
+def book_literaryfiction(request):
+    books = models.Book.objects.filter(genre="Literary Fiction").order_by("-id")
+    return render(request, "books.html", {"books": books})
+
+def book_mystery(request):
+    books = models.Book.objects.filter(genre="Mystery").order_by("-id")
+    return render(request, "books.html", {"books": books})
+
+def book_thriller(request):
+    books = models.Book.objects.filter(genre="Thriller").order_by("-id")
+    return render(request, "books.html", {"books": books})
+
+def book_historical(request):
+    books = models.Book.objects.filter(genre="Historical").order_by("-id")
+    return render(request, "books.html", {"books": books})
+
+def book_romance(request):
+    books = models.Book.objects.filter(genre="Romance").order_by("-id")
+    return render(request, "books.html", {"books": books})
+
+def book_sciencefiction(request):
+    books = models.Book.objects.filter(genre="Science Fiction").order_by("-id")
+    return render(request, "books.html", {"books": books})
+
+def book_drama(request):
+    books = models.Book.objects.filter(genre="Drama").order_by("-id")
+    return render(request, "books.html", {"books": books})
+
+def book_comedy(request):
+    books = models.Book.objects.filter(genre="Comedy").order_by("-id")
     return render(request, "books.html", {"books": books})
 
 def get_book_detail(request, id):
@@ -20,8 +63,28 @@ def add_book(request):
         form = forms.MasterpieceForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return HttpResponse("Masterpiece Created successfully!")
-            #return redirect(reverse("books_url:books_all_url"))
+            #return HttpResponse("Masterpiece Created successfully!")
+            return redirect(reverse("books_url:books_all_url"))
     else:
         form = forms.MasterpieceForm()
     return render(request, "add_books.html", {"form": form})
+
+def book_update(request, id):
+    book_object = get_object_or_404(models.Book, id=id)
+    if request.method == "POST":
+        form = forms.MasterpieceForm(instance=book_object, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("books_url:books_all_url"))
+    else:
+        form = forms.MasterpieceForm(instance=book_object)
+    return render(request, "book_update.html", {"form": form,
+                                                "object": book_object})
+
+def book_delete(request, id):
+    book_object = get_object_or_404(models.Book, id=id)
+    book_object.delete()
+    return redirect(reverse("books_url:books_all_url"))
+
+
+
